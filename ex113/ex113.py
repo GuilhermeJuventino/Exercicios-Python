9bffdefda3a58293578fe786b2a15857fdc7eeb5
Guilherme Pereira Juventino <guilhermejuventino@gmail.com>
2022-07-14 09:52:51 -0300

    Exercício 113: Funções aprofundadas em python
    Exercício 113: Re-escrever a função leiaInt() do exercício 104 agora com tratamento de erros, e tambem aproveitar e criar a função leiaFloat() que faça a mesma coisa para números flutuantes. (Tambem com tratamento de erros)

---
+++ ex113/ex113.py
@@ -0,0 +1,8 @@
+import utilidades
+
+# Programa principal
+n1 = utilidades.leiaInt("Digite um número inteiro: ")
+n2 = utilidades.leiaFloat("Digite um número flutuante: ")
+
+print(f"Você digitou o número inteiro {n1}")
+print(f"Você digitou o número flutuante {n2}")
